adf677cc372fc032c96466ff2e8f83295833144a
Adnan <adnanshakir780@gmail.com>
2026-03-09 17:54:11 +0530

    fix cors for vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,9 +14,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    process.env.FRONTEND_URL
+  ],
   credentials: true
-}))
+}));
+
+app.options("*", cors());
 
 // Routes
 app.use("/api/auth", authRouter);
