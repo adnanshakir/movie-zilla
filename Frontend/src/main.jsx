e90e87b2d68f3301f5f4c7a8b41b42e9ed64759f
Adnan <adnanshakir780@gmail.com>
2026-03-11 00:46:34 +0530

    feat: add Vercel analytics, update main entry point, and include site manifest

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-import "./styles/global.scss";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import "./styles/global.scss";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <RouterProvider router={router} />
-  </ThemeProvider>,
+  <StrictMode>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+      <Analytics />
+    </ThemeProvider>
+  </StrictMode>,
 );
